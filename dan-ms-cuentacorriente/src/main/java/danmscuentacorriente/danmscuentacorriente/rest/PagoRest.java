728a838a7adedac632e0739f64d94af25e54b7e3
Ale Guiter <aleguiter9@gmail.com>
2021-05-24 19:23:49 -0300

    Added swagger dependendencies

--- dan-ms-cuentacorriente/src/main/java/danmscuentacorriente/danmscuentacorriente/rest/PagoRest.java
+++ dan-ms-cuentacorriente/src/main/java/danmscuentacorriente/danmscuentacorriente/rest/PagoRest.java
@@ -17,10 +17,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import danmscuentacorriente.danmscuentacorriente.domain.Pago;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 
 
 @RestController
 @RequestMapping(PagoRest.API_PAGO)
+@Api(value = "PagoRest", description = "Permite gestionar los pagos")
 public class PagoRest {
     static final String API_PAGO = "/api/pago";
     
@@ -28,6 +33,7 @@ public class PagoRest {
     private static Integer ID_GEN = 1;
 
     @GetMapping(path = "/{idPago}")
+    @ApiOperation(value = "Buscar un pago por id")
     public ResponseEntity<Pago> pagoPorId(@PathVariable Integer idPago){
 
         Optional<Pago> p =  listaPagos
@@ -39,11 +45,13 @@ public class PagoRest {
     }
 
     @GetMapping
+    @ApiOperation(value = "Retorna todos los pagos")
     public ResponseEntity<List<Pago>> todos(){
         return ResponseEntity.ok(listaPagos);
     }
 
     @PostMapping
+    @ApiOperation(value = "Da de alta un pago")
     public ResponseEntity<Pago> crear(@RequestBody Pago nuevo){
     	System.out.println("Crear pago "+ nuevo);
         nuevo.setId(ID_GEN++);
@@ -51,9 +59,16 @@ public class PagoRest {
         return ResponseEntity.ok(nuevo);
     }
 
-    @PutMapping(path = "/{idPAgo}")
+    @PutMapping(path = "/{idPago}")
+    @ApiOperation(value = "Actualiza un pago")
+    @ApiResponses(value = {
+        @ApiResponse(code = 200, message = "Actualizado correctamente"),
+        @ApiResponse(code = 401, message = "No autorizado"),
+        @ApiResponse(code = 403, message = "Prohibido"),
+        @ApiResponse(code = 404, message = "El ID no existe")
+    })
     public ResponseEntity<Pago> actualizar(@RequestBody Pago nuevo,  @PathVariable Integer idPago) {
-        OptionalInt indexOpt =   IntStream.range(0, listaPagos.size())
+        OptionalInt indexOpt = IntStream.range(0, listaPagos.size())
         .filter(i -> listaPagos.get(i).getId().equals(idPago))
         .findFirst();
 
@@ -66,8 +81,9 @@ public class PagoRest {
     }
 
     @DeleteMapping(path = "/{idPago}")
+    @ApiOperation(value = "Elimina un pago")
     public ResponseEntity<Pago> borrar(@PathVariable Integer idPago){
-        OptionalInt indexOpt =   IntStream.range(0, listaPagos.size())
+        OptionalInt indexOpt = IntStream.range(0, listaPagos.size())
         .filter(i -> listaPagos.get(i).getId().equals(idPago))
         .findFirst();
 
