e8bc12a72e69bbbdbf30183c1eca6674ea477586
fio154 <51134863+fio154@users.noreply.github.com>
2021-05-24 13:50:38 -0300

    Creando dominio

---
+++ dan-ms-cuentacorriente/src/main/java/danmscuentacorriente/danmscuentacorriente/domain/Efectivo.java
@@ -0,0 +1,14 @@
+package danmscuentacorriente.danmscuentacorriente.domain;
+
+public class Efectivo extends MedioPago {
+
+	private Integer nroRecibo;
+
+	public Integer getNroRecibo() {
+		return nroRecibo;
+	}
+
+	public void setNroRecibo(Integer nroRecibo) {
+		this.nroRecibo = nroRecibo;
+	}
+}
