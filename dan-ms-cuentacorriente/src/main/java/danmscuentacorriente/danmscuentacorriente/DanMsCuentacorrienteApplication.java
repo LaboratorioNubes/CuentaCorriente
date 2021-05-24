86706445e9326686e9dfa9b2c6cae74151d8a3ea
fio154 <51134863+fio154@users.noreply.github.com>
2021-05-24 13:13:23 -0300

    Creando spring

---
+++ dan-ms-cuentacorriente/src/main/java/danmscuentacorriente/danmscuentacorriente/DanMsCuentacorrienteApplication.java
@@ -0,0 +1,13 @@
+package danmscuentacorriente.danmscuentacorriente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanMsCuentacorrienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DanMsCuentacorrienteApplication.class, args);
+	}
+
+}
---
+++ dan-ms-cuentacorriente/src/test/java/danmscuentacorriente/danmscuentacorriente/DanMsCuentacorrienteApplicationTests.java
@@ -0,0 +1,13 @@
+package danmscuentacorriente.danmscuentacorriente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanMsCuentacorrienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
