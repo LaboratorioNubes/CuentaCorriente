95089dba31fe6d8bbc833ccc4d262253981e9e33
Ale Guiter <aleguiter9@gmail.com>
2021-09-04 15:29:13 -0300

    Dockerfile created, pom updated

--- dan-ms-cuentacorriente/src/test/java/isi/dan/laboratorios/danmscuentacorriente/DanMsCuentacorrienteApplicationTests.java
+++ dan-ms-cuentacorriente/src/test/java/isi/dan/laboratorios/danmscuentacorriente/DanMsCuentacorrienteApplicationTests.java
@@ -1,4 +1,4 @@
-package danmscuentacorriente.danmscuentacorriente;
+package isi.dan.laboratorios.danmscuentacorriente;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
